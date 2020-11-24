0d11b4d3441d01b4d40b369f40a252475dc9f8e5
arsalikhov <arsalikhov@gmail.com>
2020-11-23 19:23:39 -0500

    new commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Comma.ai Challange
+
+Attempting to solve the comma.ai car speed challange
---
+++ test_player.py
@@ -0,0 +1,5 @@
+from video_processing import VideoPlayback
+
+p = VideoPlayback()
+p.play(video = r'data\test.mp4', q = 'q', k = 'k')
+
